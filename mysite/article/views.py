e1ecbc8df6a49984894d666b2455fde4f6620ecd
M Parker <mparker17@users.noreply.github.com>
2020-02-08 16:05:55 -0500

    Tell Django to render an Article model with the article.html template.

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
 # Create your views here.
+def view_article(request, id):
+    obj = get_object_or_404(Article, id=id)
+    return render(request,
+        template_name = 'article.html',
+        context={'object': obj}
+    )
