3d40324a84ce0e0b6b48f2495f847eb471e787e0
M Parker <mparker17@users.noreply.github.com>
2020-02-08 15:16:14 -0500

    Add an Article model to store Articles in memory.

--- mysite/article/models.py
+++ mysite/article/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(null=True, blank=True)
